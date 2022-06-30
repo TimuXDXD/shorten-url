aaa43d666d143c9313e30500714ecf227a3d911f
TimuXDXD <timu890518@gmail.com>
2022-06-30 21:50:25 +0800

    Initial commit

---
+++ server/app.js
@@ -0,0 +1,82 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+const cors = require("cors");
+const helmet = require("helmet");
+const compression = require("compression");
+
+const PORT = process.env.PORT || 5000;
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(compression());
+app.use((req, res, next) => {
+  res.header(
+    "Access-Control-Allow-Methods",
+    "OPTIONS, HEAD, GET, PUT, POST, DELETE"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+app.use(
+  helmet({
+    contentSecurityPolicy: false,
+  })
+);
+app.use(cors());
+app.use(cookieParser());
+app.use(express.static("./server/assets"));
+app.use(express.json({ limit: "25mb" }));
+app.use(express.urlencoded({ extended: false, limit: "25mb" }));
+if (process.env.NODE_ENV === "developement") {
+  app.use("/", express.static(__dirname + "/"));
+}
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.get('/backend', (req, res) => {
+  res.send({express: 'Backend is connected.'});
+});
+
+if (process.env.NODE_ENV === "production") {
+  // Serve any static files
+  app.use(express.static(path.resolve(__dirname, "../client/build")));
+   // Handle React routing, return all requests to React app
+   app.get("*", function (req, res) {
+    res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+  });
+}
+
+app.listen(PORT, () => console.info(`Listening on port ${PORT}`));
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
