8e75f9271830c5242fc69d6cffd2496cc4a883d6
TimuXDXD <timu890518@gmail.com>
2022-06-30 22:24:02 +0800

    Change Production env

--- server/app.js
+++ server/app.js
@@ -54,7 +54,8 @@ app.get('/backend', (req, res) => {
 
 if (process.env.NODE_ENV === "production") {
   // Serve any static files
-  app.use("/", express.static(path.resolve(__dirname, "../client/build")));
+  app.use("/", express.static(__dirname + "/"));
+  // app.use(express.static(path.resolve(__dirname, "../client/build")));
    // Handle React routing, return all requests to React app
    app.get("*", function (req, res) {
     res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
