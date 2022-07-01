48e163c63d312c717d186d90d9971081fdc8f5bc
TimuXDXD <timu890518@gmail.com>
2022-07-01 20:24:23 +0800

    Change Production env

--- server/app.js
+++ server/app.js
@@ -42,9 +42,9 @@ app.use(cookieParser());
 app.use(express.static("./server/assets"));
 app.use(express.json({ limit: "25mb" }));
 app.use(express.urlencoded({ extended: false, limit: "25mb" }));
-if (process.env.NODE_ENV === "developement") {
-  app.use("/", express.static(__dirname + "/"));
-}
+// if (process.env.NODE_ENV === "developement") {
+//   app.use("/", express.static(__dirname + "/"));
+// }
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
@@ -52,7 +52,7 @@ app.get('/backend', (req, res) => {
   res.send({express: 'Backend is connected.'});
 });
 
-if (process.env.NODE_ENV === "production") {
+// if (process.env.NODE_ENV === "production") {
   // Serve any static files
   app.use("/", express.static(__dirname + "/"));
   // app.use(express.static(path.resolve(__dirname, "../client/build")));
@@ -60,7 +60,7 @@ if (process.env.NODE_ENV === "production") {
    app.get("*", function (req, res) {
     res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
   });
-}
+// }
 
 app.listen(PORT, () => console.info(`Listening on port ${PORT}`));
 
